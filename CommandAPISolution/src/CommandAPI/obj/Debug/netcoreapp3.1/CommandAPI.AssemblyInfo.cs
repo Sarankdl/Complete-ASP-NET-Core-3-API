b4746ab615ba4306b1823a00f32fcbbc958ace7c
Sarankdl <sara.nikdl@gmail.com>
2020-10-17 13:31:12 -0700

    Added user and password to secret server

--- CommandAPISolution/src/CommandAPI/obj/Debug/netcoreapp3.1/CommandAPI.AssemblyInfo.cs
+++ CommandAPISolution/src/CommandAPI/obj/Debug/netcoreapp3.1/CommandAPI.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("affa1a7d-ab39-4cf1-8044-737acc7f9f14")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
