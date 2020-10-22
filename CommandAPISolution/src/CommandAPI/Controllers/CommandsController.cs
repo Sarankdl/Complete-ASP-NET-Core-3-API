edf06228d89f4e8a749dc2866769ef2df90ea845
Sarankdl <sara.nikdl@gmail.com>
2020-10-21 20:43:05 -0700

    Added patch to API project

--- CommandAPISolution/src/CommandAPI/Controllers/CommandsController.cs
+++ CommandAPISolution/src/CommandAPI/Controllers/CommandsController.cs
@@ -60,5 +60,25 @@ namespace CommandAPI.Controllers
             _repository.SaveChanges();
             return NoContent();
         }
+
+        [HttpPatch("{id}")]
+        public ActionResult PartialCommandUpdate(int id, JsonPatchDocument<CommandUpdateDto> patchDoc)
+        {
+            var commandModelFromRepo = _repository.GetCommandById(id);
+            if(commandModelFromRepo == null)
+            {
+                return NotFound();
+            }
+            var commandToPatch = _mapper.Map<CommandUpdateDto>(commandModelFromRepo);
+            patchDoc.ApplyTo(commandToPatch, ModelState);
+            if(!TryValidateModel(commandToPatch))
+            {
+                return ValidationProblem(ModelState);
+            }
+            _mapper.Map(commandToPatch, commandModelFromRepo);
+            _repository.UpdateCommand(commandModelFromRepo);
+            _repository.SaveChanges();
+            return NoContent();
+        }
     }
 }
