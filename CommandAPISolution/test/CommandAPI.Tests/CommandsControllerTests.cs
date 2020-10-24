ba7850d920425d11d4711866657adb2a2c044c9b
Sarankdl <sara.nikdl@gmail.com>
2020-10-23 21:07:33 -0700

    Added some unit tests

--- CommandAPISolution/test/CommandAPI.Tests/CommandsControllerTests.cs
+++ CommandAPISolution/test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -23,8 +23,7 @@ namespace CommandAPI.Tests
         {
             mockRepo = new Mock<ICommandAPIRepo>();
             realProfile = new CommandsProfile();
-            configuration = new MapperConfiguration(cfg => cfg.
-            AddProfile(realProfile));
+            configuration = new MapperConfiguration(cfg => cfg.AddProfile(realProfile));
             mapper = new Mapper(configuration);
         }
 
@@ -39,10 +38,36 @@ namespace CommandAPI.Tests
         [Fact]
         public void GetCommandItems_Returns200OK_WhenDBIsEmpty()
         {
+            mockRepo.Setup(repo => repo.GetAllCommands()).Returns(GetCommands(0));
             var controllers = new CommandsController(mockRepo.Object, mapper);
 
             var result = controllers.GetAllCommands();
 
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void GetAllCommands_ReturnsOneItem_WhenDBHasOneResource()
+        {
+            mockRepo.Setup(repo => repo.GetAllCommands()).Returns(GetCommands(1));
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+            var result = controller.GetAllCommands();
+
+            var okResult = result.Result as OkObjectResult;
+            var commands = okResult.Value as List<CommandReadDto>;
+            Assert.Single(commands);
+        }
+
+        [Fact]
+        public void GetAllCommands_Returns200OK_WhenDBHasOneResource()
+        {
+            //Arrange
+            mockRepo.Setup(repo => repo.GetAllCommands()).Returns(GetCommands(1));
+            var controller = new CommandsController(mockRepo.Object, mapper);
+            //Act
+            var result = controller.GetAllCommands();
+            //Assert
             Assert.IsType<OkObjectResult>(result.Result);
         }
 
