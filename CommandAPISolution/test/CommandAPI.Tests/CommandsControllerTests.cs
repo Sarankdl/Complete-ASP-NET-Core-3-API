1f5eb6cdbb0540da05ebc34c0541fbfa14d2e3de
Sarankdl <sara.nikdl@gmail.com>
2020-10-23 20:33:20 -0700

    Added first test for controller

---
+++ CommandAPISolution/test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -0,0 +1,65 @@
+using System;
+using Xunit;
+using CommandAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Moq;
+using AutoMapper;
+using CommandAPI.Models;
+using CommandAPI.Data;
+using CommandAPI.Profiles;
+using CommandAPI.Dtos;
+
+namespace CommandAPI.Tests
+{
+    public class CommandsControllerTests : IDisposable
+    {
+        Mock<ICommandAPIRepo> mockRepo;
+        CommandsProfile realProfile;
+        MapperConfiguration configuration;
+        IMapper mapper;
+
+        public CommandsControllerTests()
+        {
+            mockRepo = new Mock<ICommandAPIRepo>();
+            realProfile = new CommandsProfile();
+            configuration = new MapperConfiguration(cfg => cfg.
+            AddProfile(realProfile));
+            mapper = new Mapper(configuration);
+        }
+
+        public void Dispose()
+        {
+            mockRepo = null;
+            mapper = null;
+            configuration = null;
+            realProfile = null;
+        }
+
+        [Fact]
+        public void GetCommandItems_Returns200OK_WhenDBIsEmpty()
+        {
+            var controllers = new CommandsController(mockRepo.Object, mapper);
+
+            var result = controllers.GetAllCommands();
+
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+
+        private List<Command> GetCommands(int num)
+        {
+            var commands = new List<Command>();
+            if (num > 0)
+            {
+                commands.Add(new Command 
+                {
+                    Id = 0,
+                    HowTo = "How to generate a migration",
+                    CommandLine = "dotnet ef migrations add <Name of Migration>",
+                    Platform = ".Net Core EF"
+                });
+            }
+            return commands;
+        }
+    }
+}
